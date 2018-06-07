f9d0210e5e2c13e85150a1495a04b93ef8ba6e5d
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-06-07 19:34:49 -0400

    Implemented Piece Inteface -- Edit later

---
+++ src/chess/Piece.java
@@ -0,0 +1,18 @@
+
+/* This interface will contain the foundations for all the pieces in the Chess Game */
+public interface Piece{
+
+  // take care of bound check, check if person puts piece back in new spot as well
+  boolean validMove(int newX, int newY);
+  // check if there is a piece there or not (opponent or same side) + is it valid?
+  boolean validAttack(int newX, int newY);
+  // black is true, white is false
+  boolean color;
+  // hold current x values
+  int currentX;
+  // hold current y value
+  int currentY;
+  // if it is still in game or not
+  boolean deleted;
+
+}
