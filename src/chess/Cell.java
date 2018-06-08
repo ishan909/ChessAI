6912e40a1f0228afbd24c7de7d69b6a5f4ce67e2
ishan909 <ishan909@gmail.com>
2018-06-07 21:18:49 -0500

    Temp cell class

--- src/chess/Cell.java
+++ src/chess/Cell.java
@@ -5,8 +5,31 @@ class Cell {
     private boolean containsPiece;
     private Piece currentPiece;
 
-    public Cell(Piece piece) {
-        this.here = piece;
+    public Cell(int x, int y, boolean addPiece, Piece piece) {
+        this.x = x;
+        this.y = y;
+        this.containsPiece = addPiece;
+        if (addPiece) {
+            this.currentPiece = piece;
+        } else {
+            currentPiece = new Piece();
+        }
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public boolean containsPiece() {
+        return this.containsPiece;
+    }
+
+    public Piece getPiece() {
+        return this.currentPiece;
     }
 
 }
