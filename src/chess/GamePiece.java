864862ad96aaed2109c978ba86ccec756e4abf5c
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-06-15 21:59:32 -0400

    interface functionality add as needed

--- src/chess/GamePiece.java
+++ src/chess/GamePiece.java
@@ -2,9 +2,16 @@
 
 public interface GamePiece {
 
-  boolean isInCheck();
-  boolean validMove();
-  
+  // check (king only)
+  public boolean isInCheck();
+  // checks validity
+  public boolean validMove();
+  // returns type of piece as String
+  public String getType();
+  // can't take your own piece, valid square to move on
+  public boolean canAttack();
+  // alive
+  public boolean alive();
 
 
 }
