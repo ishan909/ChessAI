d2cbbe727e03df39a915c48c81ce4f88539ec978
ishan909 <ishan909@gmail.com>
2018-06-15 21:44:35 -0500

    updated to an abstract game piece

--- src/chess/GamePiece.java
+++ src/chess/GamePiece.java
@@ -1,19 +1,34 @@
 
 
-public interface GamePiece {
+public abstract class GamePiece {
+    // location of the piece
+    public int x, y;
+    // color of the piece: black = true, red = false;
+    public boolean color;
+    // if this is the first move of the piece
+    public boolean firstMove;
+    // if the piece is alive
+    public boolean isAlive;
 
-  // check (king only)
-  public boolean isInCheck();
-  // checks validity
-  public boolean validMove();
-  // returns type of piece as String
-  public String getType();
-  // can't take your own piece, valid square to move on
-  public boolean canAttack();
-  // alive
-  public boolean alive();
+    public GamePiece(int x, int y, boolean color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        firstMove = true;
+    }
 
-
+    // check (king only)
+    public boolean isInCheck();
+    // checks validity
+    public boolean validMove();
+    // returns type of piece as String
+    public String getType();
+    // can't take your own piece, valid square to move on
+    public boolean canAttack();
+    // alive
+    public boolean isAlive() {
+        return this.isAlive;
+    }
 }
 
 // alive parameter, int x, int y, getType
