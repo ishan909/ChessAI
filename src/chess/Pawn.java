25de73d8c521c30199c20b3ba2888097fe238672
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-06-18 23:28:11 -0400

    changes to pawn class

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -28,14 +28,96 @@ public class Pawn extends GamePiece {
      * @return if piece can attack a piece
      */
     public boolean canMove(int x, int y, Board board) {
+
         // make sure you can not attack your own piece
         if (x == currentX && y == currentY) {
             return false;
         }
+        // valid position
         if (x < 0 || x > 7 || y < 0 || y > 7) {
             return false;
         }
+
         // attack
         // TODO finish implementing attack
+
+        if(!firstMove) {
+          // can only move up once, do by colors
+
+          // white color
+          if(!color) {
+            // make sure you can only go up
+            if(y - currentY != 1) {
+              return false;
+            }
+            // check for dx
+            if(x-currentX == 1 || x-currentX == -1) {
+              if(y - currentY == 1) {
+                return this.color != board.getPiece(x, y).color;
+              }
+            }
+
+          }
+          // black color
+          else {
+            if(y - currentY != -1) {
+              return false;
+            }
+
+            // check for dx
+            if(x - currentX == 1 || x-currentX == -1) {
+              if(y - currentY == -1) {
+                return this.color != board.getPiece(x, y).color;
+              }
+            }
+
+          }
+        }
+        else {
+          // move up 2 or 1
+
+          // update, in another function
+
+          // white color
+          if(!color) {
+            // make sure you can only go up
+            if(y - currentY != 1 || y-currentY != 2) {
+              return false;
+            }
+            // check for dx
+            if(x-currentX == 1 || x-currentX == -1) {
+              if(y - currentY == 1) {
+                return this.color != board.getPiece(x, y).color;
+              }
+              else {
+                return false;
+              }
+            }
+
+          }
+          // black color
+          else {
+            if(y - currentY != -1 || y - currentY != -2) {
+              return false;
+            }
+
+            // check for dx
+            if(x - currentX == 1 || x-currentX == -1) {
+              if(y - currentY == -1) {
+                return this.color != board.getPiece(x, y).color;
+              }
+              else {
+                return false;
+              }
+            }
+
+          }
+
+
+        }
+
+
+
     }
+
 }
