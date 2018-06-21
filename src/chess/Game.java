35ecc95e58d98bb1d957a1087740e56b0d515367
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-06-20 20:26:44 -0400

    updated check

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -14,7 +14,7 @@ public class Game {
     public playGame(Board board) {
         Scanner input = new Scanner(System.in);
         // Initialize move counter
-        int moveCounter = 0;
+        moveCount = 0;
 
         while (true) { // true for the black player
             if (!board.checkmate(true)) {
@@ -23,6 +23,8 @@ public class Game {
                     System.out.println("Black, you are in check");
                 }
                 System.out.print("Black, which piece would you like to move? Enter horizonal first, then vertical: ");
+
+
                 // TODO: make sure input is valid -- must make sure there is a piece there,
                 //      that piece is that player's piece, and the string is formatted correctly (letter + num)
                 int currentHorizontal = input.nextInt();
@@ -34,7 +36,8 @@ public class Game {
                 // keep in mind that this returns a boolean to tell you if the move was successful
 
                 // TODO: figure out how to work with bad input (throw exceptions later)
-                while (!board.movePiece(currentHorizontal, currentVertical, new_horizontal, new_vertical, moveCounter)) {
+                while (!board.movePiece(currentHorizontal, currentVertical, new_horizontal, new_vertical, moveCounter) || board.check(true)) {
+
                     System.out.println("Invalid Move. ");
                     System.out.print("Black, which piece would you like to move? Enter horizonal first, then vertical: ");
                     int currentHorizontal = input.nextInt();
@@ -43,7 +46,7 @@ public class Game {
                     int new_horizontal = input.nextInt();
                     int new_vertical = input.nextInt();
                 }
-                System.out.println("Black, your peice has been moved.");
+                System.out.println("Black, your piece has been moved.");
                 moveCounter += 1;
             } else {
                 break;
@@ -63,7 +66,7 @@ public class Game {
                 int new_vertical = input.nextInt();
                 // TODO: Update the board with the user's move (added to the board class)
                 // keep in mind that this returns a boolean to tell you if the move was successful
-                while (!board.movePiece(currentHorizontal, currentVertical, new_horizontal, new_vertical, moveCounter)) {
+                while (!board.movePiece(currentHorizontal, currentVertical, new_horizontal, new_vertical, moveCounter) || board.check(true)) {
                     System.out.println("Invalid Move. ");
                     System.out.print("Red, which piece would you like to move? Enter horizonal first, then vertical: ");
                     int currentHorizontal = input.nextInt();
@@ -72,7 +75,7 @@ public class Game {
                     int new_horizontal = input.nextInt();
                     int new_vertical = input.nextInt();
                 }
-                System.out.println("Red, your peice has been moved.");
+                System.out.println("Red, your piece has been moved.");
                 moveCounter += 1;
             } else {
                 break;
