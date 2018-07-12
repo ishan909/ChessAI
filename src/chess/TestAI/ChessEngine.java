a7290aa3767e3ab4055475ad58289bd352a02089
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-07-11 23:44:58 -0400

    test ai

---
+++ src/chess/TestAI/ChessEngine.java
@@ -0,0 +1,21 @@
+/* This will be a test class for the eventual AI */
+
+public class ChessEngine {
+
+  public static void main(String[] args) {
+
+  // Assign weights?
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+}
