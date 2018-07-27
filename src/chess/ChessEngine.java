dbd86bd47b6b848a3437a30833a6378e5b565906
Sriram Rameshbabu <sriramrameshbabu98@gmail.com>
2018-07-26 20:54:21 -0500

    deletions

--- src/chess/ChessEngine.java
+++ src/chess/ChessEngine.java
@@ -2,27 +2,9 @@
 
 public class ChessEngine {
 
-  // Minimax algorithm, generating a search tree
-
-  /* Try to do a random algorithm first
-   * Then assign weights to the pieces
-   * Pick a color for the AI and do it in that one only
-   *
-   *
-   *
-   */
-
-   // change weights
-
-   // King - 1000
-   // Queen - 300
-   // Rook - 150
-   // Bishop - 60
-   // Knight - 60
-   // Pawn - 10
+  
 
 
-   
 
 
 
