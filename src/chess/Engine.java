5bcfee5399eac301ba13ee4951ea33d969a55e82
ishan909 <ishan909@gmail.com>
2018-08-18 23:12:57 -0500

    updated tie breaker

--- src/chess/Engine.java
+++ src/chess/Engine.java
@@ -21,7 +21,6 @@ public class Engine {
 		return findBestMove();
 	}
 	
-	
 	/* Generate all the possible moves */
 	private ArrayList<Integer[]> possibleMoves() {
 		ArrayList<Integer[]> moves = new ArrayList<Integer[]>();
@@ -61,32 +60,29 @@ public class Engine {
 	// This function will have to calculate the best move (hopefully based on 1 move forward)
 	// In order to repeat for more moves, we need to apply or create our own recursion stack or iteration of some sort
 	public Integer[] findBestMove() {
-		ArrayList<Integer[]> possible_moves = possibleMoves();
 		int highest = Integer.MIN_VALUE;
-		int index = -1;
-		// iterate through the arraylist of possible moves and find the best move
-		for(int i = 0; i < possible_moves.size(); i++) {
+		ArrayList<Integer[]> possible_moves = possibleMoves();
+		ArrayList<Integer> highestTies = new ArrayList<Integer>();
+		// iterate through the ArrayList of possible moves and find the best move
+		for (int i = 0; i < possible_moves.size(); i++) {
 			/* Algorithm:
-			 * Evaluate the array at each index of the arraylist using a helper evaluate function that creates a copy of the board and evaluates it
+			 * Evaluate the array at each index of the ArrayList using a helper evaluate function that creates a copy of the board and evaluates it
 			 * Use the normal max algorithm to find the best scoring one
 			 * Return the index of the best one (if tie-breaker, just return 1st --> should be done automatically)
 			 */
 			
-			// Skeleton of Algorithm below
 			int current = evaluateBoard(possible_moves.get(i)[0], possible_moves.get(i)[1], possible_moves.get(i)[2], possible_moves.get(i)[3]);
 			if (highest <= current) {
 				if (highest == current) {
-					if (Math.random() < 0.4) {
-						highest = current;
-						index = i;
-					}
+					highestTies.add(i);
 				} else {
+					highestTies = new ArrayList<Integer>();
+					highestTies.add(i);
 					highest = current;
-					index = i;
 				}
 			}
 		}
-		return possible_moves.get(index);
+		return possible_moves.get(highestTies.get((int) (Math.random() * highestTies.size())));
 	}
 	
 	/* Big Idea
@@ -154,164 +150,4 @@ public class Engine {
 		}
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	/*-----------------------------------------------------------------------------------------------------------------------------------------*/
-	
-//	public Integer[] findBestMove() {
-//		// first get all the possible moves at a current level
-//		ArrayList<Integer[]> possible = possibleMoves();
-//		int highest = -9999;
-//		int index = -1;
-//		for (int i = 0; i < possible.size(); i++) {
-//			Board[] tmp = generate(possible.get(i)[0], possible.get(i)[1], possible.get(i)[2], possible.get(i)[3]);
-//			int score = currentBlackScore(tmp[1]);
-//			if (highest < score) {
-//				index = i;
-//			}
-//		}
-//		return possible.get(index);
-//	}
-	
-//	// convert array of boards to stack of boards if it works, do null checks
-//	public Board[] generate(int current_row, int current_col, int new_row, int new_col) {
-//		Board[] boards = new Board[2];
-//		boards[0] = new Board();
-//		boards[1] = new Board();
-//		for (int i = 0; i < 8; i++) {
-//			for (int j = 0; j < 8; j++) {
-//				boards[0].matrix[i][j] = game.getBoard().matrix[i][j];
-//				boards[1].matrix[i][j] = game.getBoard().matrix[i][j];
-//				
-//				GamePiece p = game.getBoard().matrix[i][j];
-//				if (p == null) {
-//					continue;
-//				} else if (p instanceof Pawn) {
-//					boards[0].matrix[i][j] = new Pawn(i, j, p.color);
-//					boards[1].matrix[i][j] = new Pawn(i, j, p.color);
-////					boards[0].matrix[i][j].firstMove = p.firstMove;
-////					boards[1].matrix[i][j].firstMove = p.firstMove;
-//				} else if (p instanceof Rook) {
-//					boards[0].matrix[i][j] = new Rook(i, j, p.color);
-//					boards[1].matrix[i][j] = new Rook(i, j, p.color);
-////					boards[0].matrix[i][j].firstMove = p.firstMove;
-////					boards[1].matrix[i][j].firstMove = p.firstMove;
-//				} else if (p instanceof Knight) {
-//					boards[0].matrix[i][j] = new Knight(i, j, p.color);
-//					boards[1].matrix[i][j] = new Knight(i, j, p.color);
-////					boards[0].matrix[i][j].firstMove = p.firstMove;
-////					boards[1].matrix[i][j].firstMove = p.firstMove;
-//				} else if (p instanceof Bishop) {
-//					boards[0].matrix[i][j] = new Bishop(i, j, p.color);
-//					boards[1].matrix[i][j] = new Bishop(i, j, p.color);
-////					boards[0].matrix[i][j].firstMove = p.firstMove;
-////					boards[1].matrix[i][j].firstMove = p.firstMove;
-//				} else if (p instanceof Queen) {
-//					boards[0].matrix[i][j] = new Queen(i, j, p.color);
-//					boards[1].matrix[i][j] = new Queen(i, j, p.color);
-////					boards[0].matrix[i][j].firstMove = p.firstMove;
-////					boards[1].matrix[i][j].firstMove = p.firstMove;
-//				} else if (p instanceof King) { // instanceof King
-//					boards[0].matrix[i][j] = new King(i, j, p.color);
-//					boards[1].matrix[i][j] = new King(i, j, p.color);
-////					boards[0].matrix[i][j].firstMove = p.firstMove;
-////					boards[1].matrix[i][j].firstMove = p.firstMove;
-//				}
-//			}
-//		}
-//		for (int i = 0; i < 2; i++) {
-//			boards[0].blackKingLocation[i] = game.getBoard().blackKingLocation[i];
-//			boards[0].whiteKingLocation[i] = game.getBoard().whiteKingLocation[i];
-//			boards[1].blackKingLocation[i] = game.getBoard().blackKingLocation[i];
-//			boards[1].whiteKingLocation[i] = game.getBoard().whiteKingLocation[i];
-//		}
-//		GamePiece tmp =	boards[1].getPiece(current_row, current_col); // index 0 is original and 1 is the one we are changing
-//		if (tmp != null) {
-//			// Does it actually move it on boards[1]?
-//			
-//			// Before
-//			System.out.println("This is before");
-//			//printBoard(boards[1]);
-//			if (tmp.canMove(new_row, new_col, boards[1])) {
-//				System.out.println("This is after");
-//				tmp.move(new_row, new_col, boards[1]);
-//			//	printBoard(boards[1]);
-//				
-//			}
-//		}
-//		
-//		return boards;
-//	}
-	
-//	public void printBoard(Board board) {
-//		 System.out.println(" - 0- 1- 2- 3- 4- 5- 6- 7-");
-//	        System.out.println("---------------------------");
-//	        for (int row = 0; row < 8; row++) {
-//	            System.out.print("" + row + "|");
-//	            for (int col = 0; col < 8; col++) {
-//	                if (board.matrix[row][col] == null) {
-//	                    System.out.print("  ");
-//	                } else {
-//	                    GamePiece p = board.matrix[row][col];
-//	                    if (p == null) {
-//	            			System.out.print("  |");
-//	            			continue;
-//	                    }
-//	                    if (p.getColor()) {
-//	                        System.out.print("B");
-//	                    } else {
-//	                        System.out.print("W");
-//	                    }
-//	                    if (p instanceof Rook) {
-//	                        System.out.print("R");
-//	                    } else if (p instanceof Knight) {
-//	                        System.out.print("k");
-//	                    } else if (p instanceof Bishop) {
-//	                        System.out.print("B");
-//	                    } else if (p instanceof King) {
-//	                        System.out.print("K");
-//	                    } else if (p instanceof Queen) {
-//	                        System.out.print("Q");
-//	                    } else if (p instanceof Pawn) {
-//	                        System.out.print("P");
-//	                    }
-//	                }
-//	                System.out.print("|");
-//	            }
-//	            System.out.println();
-//	        }
-//	        System.out.println("---------------------------");
-//	}
-	
-//	/* Not needed but here in case we revert back */
-//	private int getPoints(Integer[] list) {
-//		GamePiece p = game.getBoard().getPiece(list[2], list[3]);
-//		if (p == null) {
-//			return -1;
-//		} else {
-//			if (p instanceof Pawn) {
-//				return pawnWeight;
-//			} else if (p instanceof Knight) {
-//				return knightWeight;
-//			} else if (p instanceof Bishop) {
-//				return bishopWeight;
-//			} else if (p instanceof Rook) {
-//				return rookWeight;
-//			} else if (p instanceof Queen) {
-//				return queenWeight;
-//			} else {
-//				return kingWeight;
-//			}
-//		}
-//	}
 }
